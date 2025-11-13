d4573d525f4f2ceba1b1e72e6cd04fc664a8b1e8
Aasu703 <smellylips508@gmail.com>
2025-11-13 09:53:37 +0545

    Arithmetic operations

--- lib/screens/ArithmeticOperations.dart
+++ lib/screens/ArithmeticOperations.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Arithmeticoperations extends StatelessWidget {
+class Arithmeticoperations extends StatefulWidget {
   const Arithmeticoperations({super.key});
 
+  @override
+  State<Arithmeticoperations> createState() => _ArithmeticoperationsState();
+}
+
+class _ArithmeticoperationsState extends State<Arithmeticoperations> {
+  int first = 0;
+  int second = 0;
+  int result = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +19,49 @@ class Arithmeticoperations extends StatelessWidget {
         title: Text("Arithmetic Operations", style: TextStyle(fontSize: 20)),
         backgroundColor: Colors.purpleAccent,
         titleSpacing: 85,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(2),
+        child: Column(
+          children: [
+            SizedBox(height: 12),
+            TextField(
+              keyboardType: TextInputType.number,
+              onChanged: (value) => {first = int.tryParse(value) ?? 0},
+              decoration: InputDecoration(
+                label: Text("Enter your first number:"),
+                hintText: ("eg...55"),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 12),
+            TextField(
+              keyboardType: TextInputType.number,
+              onChanged: (value) => {second = int.tryParse(value) ?? 0},
+              decoration: InputDecoration(
+                label: Text("Enter your second number:"),
+                hintText: ("eg...55"),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 12),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = first + second;
+                  });
+                },
+                child: Text("Calculate", style: TextStyle(fontSize: 20)),
+              ),
+            ),
+
+            SizedBox(height: 12),
+            Text("Total is: $result", style: TextStyle(fontSize: 20)),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4,9 +4,14 @@ import 'package:firstproject/screens/Armstrong.dart';
 import 'package:firstproject/screens/Palindrome.dart';
 import 'package:flutter/material.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(

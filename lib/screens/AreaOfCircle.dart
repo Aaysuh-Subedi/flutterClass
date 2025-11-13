223c1a054c07f7038b78c4649ff3cb4fcb3371ec
Aasu703 <smellylips508@gmail.com>
2025-11-13 16:08:57 +0545

    Final Commits

--- lib/screens/AreaOfCircle.dart
+++ lib/screens/AreaOfCircle.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-class Areaofcircle extends StatelessWidget {
+class Areaofcircle extends StatefulWidget {
   const Areaofcircle({super.key});
 
+  @override
+  State<Areaofcircle> createState() => _AreaofcircleState();
+}
+
+class _AreaofcircleState extends State<Areaofcircle> {
+  //controller
+  final TextEditingController radius = TextEditingController(text: "");
+  // final key
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+  double result = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +20,50 @@ class Areaofcircle extends StatelessWidget {
         title: Text("Area of Circle", style: TextStyle(fontSize: 20)),
         backgroundColor: Colors.cyan,
         titleSpacing: 100,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(9),
+        child: Form(
+          key: _globalKey,
+          child: Column(
+            children: [
+              SizedBox(height: 12),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: radius,
+                decoration: InputDecoration(
+                  label: Text("Enter your number:"),
+                  hintText: ("eg radius=2.5"),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 12),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_globalKey.currentState!.validate()) {
+                      double num = double.parse(radius.text);
+                      setState(() {
+                        result = 3.14 * num * num;
+                      });
+                    }
+                  },
+                  child: Text("Calculate", style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              SizedBox(height: 12),
+              Text("Area of Radius: $result", style: TextStyle(fontSize: 20)),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/ArithmeticOperations.dart
+++ lib/screens/ArithmeticOperations.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class Arithmeticoperations extends StatefulWidget {
   const Arithmeticoperations({super.key});
@@ -11,12 +10,15 @@ class Arithmeticoperations extends StatefulWidget {
 class _ArithmeticoperationsState extends State<Arithmeticoperations> {
   //controller
   final TextEditingController firstController = TextEditingController(
-    text: "50",
+    text: "55",
   );
 
   final TextEditingController secondContoller = TextEditingController(
-    text: "31",
+    text: "55",
   );
+
+  // Global key
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   int result = 0;
   @override
   Widget build(BuildContext context) {
@@ -28,49 +30,67 @@ class _ArithmeticoperationsState extends State<Arithmeticoperations> {
       ),
       body: Padding(
         padding: EdgeInsets.all(2),
-        child: Column(
-          children: [
-            SizedBox(height: 12),
-            TextField(
-              keyboardType: TextInputType.number,
-              // onChanged: (value) => {first = int.tryParse(value) ?? 0},
-              controller: firstController,
-              decoration: InputDecoration(
-                label: Text("Enter your first number:"),
-                hintText: ("eg...55"),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              keyboardType: TextInputType.number,
-              // onChanged: (value) => {second = int.tryParse(value) ?? 0},
-              controller: secondContoller,
-              decoration: InputDecoration(
-                label: Text("Enter your second number:"),
-                hintText: ("eg...55"),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 12),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    result =
-                        (int.tryParse(firstController.text) ?? 0) +
-                        (int.tryParse(secondContoller.text) ?? 0);
-                  });
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              SizedBox(height: 12),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                // onChanged: (value) => {first = int.tryParse(value) ?? 0},
+                controller: firstController,
+                decoration: InputDecoration(
+                  label: Text("Enter your first number:"),
+                  hintText: ("eg...55"),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter  number';
+                  }
+                  return null;
                 },
-                child: Text("Calculate", style: TextStyle(fontSize: 20)),
               ),
-            ),
+              SizedBox(height: 12),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                // onChanged: (value) => {second = int.tryParse(value) ?? 0},
+                controller: secondContoller,
+                decoration: InputDecoration(
+                  label: Text("Enter your second number:"),
+                  hintText: ("eg...55"),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter  number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 12),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      // valid
+                      int firstNo = int.parse(firstController.text);
+                      int secondNo = int.parse(secondContoller.text);
+                      setState(() {
+                        result = firstNo + secondNo;
+                      });
+                    }
+                  },
+                  child: Text("Calculate", style: TextStyle(fontSize: 20)),
+                ),
+              ),
 
-            SizedBox(height: 12),
-            Text("Total is: $result", style: TextStyle(fontSize: 20)),
-          ],
+              SizedBox(height: 12),
+              Text("Total is: $result", style: TextStyle(fontSize: 20)),
+            ],
+          ),
         ),
       ),
     );
